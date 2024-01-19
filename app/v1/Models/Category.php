dec9147446025b08b4fcbad5e37330e5aa608d05
comment <obrazovsergey@gmail.com>
2024-01-19 14:55:34 +0300

    add categories and items swagger controller and routes

---
+++ app/v1/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\v1\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'id', 'title', 'parent_id', 'isActive', 'isDeleted'
+    ];
+    protected $guarded = ['id'];
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class,'category_id');
+    }
+
+}
