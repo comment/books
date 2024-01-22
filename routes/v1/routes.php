e00ae86c7e45f78294d63647c58bd08ff5aa7044
comment <obrazovsergey@gmail.com>
2024-01-22 12:25:16 +0300

    add sanctum auth

--- routes/v1/routes.php
+++ routes/v1/routes.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('sanctum')->as('sanctum:')->group(
+    base_path('routes/v1/auth.php'),
+);
+
 Route::prefix('categories')->as('categories:')->group(
     base_path('routes/v1/categories.php'),
 );
 
 Route::prefix('items')->as('items:')->group(
     base_path('routes/v1/items.php'),
-);
-
-Route::prefix('books')->as('books:')->group(
-    base_path('routes/v1/books.php'),
 );
